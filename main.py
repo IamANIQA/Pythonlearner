53da1b708f3df75ccc0c1cef65a0889418eaaad5
Aniqa Tabassum <aniqa.t600@gmail.com>
2024-03-05 05:24:42 +0000

    Adding two numbers

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print ("Day 1 - Python Print Function")
-print ("The function is declared like this:")
-print ("print ('what to print')")
+print ("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+
+print ("Your band name could be " + city + " " + pet)
