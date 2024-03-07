acb8d1a0fc15893d3a519a8e32a92f172d155505
Aniqa Tabassum <aniqa.t600@gmail.com>
2024-03-07 04:58:14 +0000

    The Hurdles Loop Challenge

---
+++ Day06/Ex01.py
@@ -0,0 +1,22 @@
+# The Hurdles Loop Challenge
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+for start in range(0, 6):
+    jump()
